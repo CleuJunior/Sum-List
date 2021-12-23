ebee4d73af65ef0b5c0bd349aa160fe54919c356
Cleonildo Junior <cleu.junior@gmail.com>
2021-12-23 12:29:20 -0300

    Projeto Final

--- sumlist/lib/sumlist.ex
+++ sumlist/lib/sumlist.ex
@@ -1,6 +1,10 @@
 defmodule Sumlist do
   def call(list), do: sum(list, 0)
 
+  def call_enum(list), do: Enum.any?(list, fn echElem -> echElem > 5 end)
+
+  def call_sort_number(list), do: Enum.sort(list)
+
   defp sum([], acc), do: acc
 
   defp sum([head | tail], acc) do
